f2b47e2f739f88fd719f547f437c3effa15fd0fd
Jose Carlos Alarcon <joseadp5@gmail.com>
2023-11-04 18:01:42 -0400

    vista de inicio , login y registrar

---
+++ lib/screens/screens.dart
@@ -0,0 +1,3 @@
+export 'package:dress_me_up/screens/welcome_screen.dart';
+export 'package:dress_me_up/screens/login_screen.dart';
+export 'package:dress_me_up/screens/register_screen.dart';
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,3 @@
+export 'package:dress_me_up/widgets/custom_filled_button.dart';
+export 'package:dress_me_up/widgets/custom_text_form_field.dart';
+export 'package:dress_me_up/widgets/geometrical_background.dart';
